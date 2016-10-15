87641729e84a6c5f5dc1358f2ee27e1e433fd7a1
Drake Ballard <drakeballard@gmail.com>
2016-10-15 12:54:45 -0500

    twitter is properly linked!

--- liri.js
+++ liri.js
@@ -1,14 +1,31 @@
 // Liri needs to pull data from the key.js file
 var keys = require('./keys.js');
 
-// get all the different variables from the jey.js
-var twitterKeys = keys.twitterKeys;
-var spotifyKeys = keys.spotifyKeys;
 
-// console logging this right now, but it may be deleted due to the fact that it reveals information
-for (var key in twitterKeys) {
-  console.log("Twitter key: " + twitterKeys[key]);
+var Twitter = require('twitter');
+var Spotify = require('spotify');
+var client = new Twitter(keys.twitterKeys);
+var fs = require('fs');
+//
+// console.log(keys.twitterKeys);
+
+function twitter(){
+  var params = {screen_name: 'drakeba11', count: 20, trim_user: true};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      for(var i = 0; i < tweets.length; i++) {
+        console.log(tweets[i].created_at + " : " + tweets[i].text);
+        console.log("=============================================");
+      }
+      } else {
+        console.log(error);
+      }
+
+});
 };
-for (var key in spotifyKeys) {
-  console.log("Spotify key: " + spotifyKeys[key]);
+
+
+
+if(process.argv[2] == "my-tweets") {
+  twitter();
 };
