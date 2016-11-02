6a83fe2bc8a022f4686d5970c302bd291dd24ba2
Drake Ballard <drakeballard@gmail.com>
2016-11-01 23:40:12 -0500

    twitter now working

--- liri.js
+++ liri.js
@@ -1,7 +1,7 @@
 // Liri needs to pull data from the key.js file
 var keys = require('./keys.js');
 
-
+//creating dependencies
 var Twitter = require('twitter');
 var Spotify = require('spotify');
 var client = new Twitter(keys.twitterKeys);
@@ -9,7 +9,8 @@ var fs = require('fs');
 //
 // console.log(keys.twitterKeys);
 
-function twitter(){
+
+function twitter(  ){
   var params = {screen_name: 'drakeba11', count: 20, trim_user: true};
   client.get('statuses/user_timeline', params, function(error, tweets, response) {
     if (!error) {
@@ -17,15 +18,28 @@ function twitter(){
         console.log(tweets[i].created_at + " : " + tweets[i].text);
         console.log("=============================================");
       }
-      } else {
-        console.log(error);
       }
 
 });
+}
+
+var pick = function(caseData, functionData) {
+  switch(caseData) {
+    case 'my-tweets':
+      twitter( );
+      break;
+    default:
+    console.log('LIRI DOES NOT KNOW HOW TO DO THAT');
+
+  }
+}
+
+var runThis = function(argOne, argTwo) {
+  pick(argOne, argTwo);
 };
 
+runThis(process.argv[2], process.argv[3]);
 
-
-if(process.argv[2] == "my-tweets") {
-  twitter();
-};
+// if(process.argv[2] == "my-tweets") {
+//   twitter();
+// };
